5ce492bc8119a5c2c781bef24566bbf1fdd576b5
Brandon Woodruff <brandonwoodruff92@gmail.com>
2025-07-25 21:55:44 -0400

    Requires modules in top level module

--- lib/rails_rest_kit.rb
+++ lib/rails_rest_kit.rb
@@ -1,6 +1,8 @@
 require "rails-rest_kit/version"
 require "rails-rest_kit/helpers/resource_helper"
+require "rails-rest_kit/helpers/permitter_helper"
 require "rails-rest_kit/restful_controller_actions"
+require "rails-rest_kit/permitter"
 require "rails-rest_kit/engine" if defined?(Rails)
 
 module RailsRestKit
